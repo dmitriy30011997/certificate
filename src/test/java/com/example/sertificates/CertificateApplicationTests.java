48c1c56c8e87e50c9bfa1396206724649cee05c4
dmitriy30011997 <112504672+dmitriy30011997@users.noreply.github.com>
2024-01-26 11:42:53 +0300

    CertificateEntity created

---
+++ src/test/java/com/example/sertificates/CertificateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sertificates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
