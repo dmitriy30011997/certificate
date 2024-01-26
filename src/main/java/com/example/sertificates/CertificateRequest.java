288e6a313c0e74345c462330874b08ed64a0f365
dmitriy30011997 <112504672+dmitriy30011997@users.noreply.github.com>
2024-01-26 14:08:32 +0300

    request created

---
+++ src/main/java/com/example/sertificates/CertificateRequest.java
@@ -0,0 +1,50 @@
+package com.example.sertificates;
+
+public class CertificateRequest {
+
+    private String username;
+    private String encryptedPublicKey;
+    private String otherData;
+    private String publicKey;
+    private String signature;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEncryptedPublicKey() {
+        return encryptedPublicKey;
+    }
+
+    public void setEncryptedPublicKey(String encryptedPublicKey) {
+        this.encryptedPublicKey = encryptedPublicKey;
+    }
+
+    public String getOtherData() {
+        return otherData;
+    }
+
+    public void setOtherData(String otherData) {
+        this.otherData = otherData;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+}
