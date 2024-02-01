dd022278a67ffeac3407cd810a60456477dc3347
dmitriy30011997 <112504672+dmitriy30011997@users.noreply.github.com>
2024-02-01 11:10:31 +0300

    endpoint verify created

--- src/main/java/com/example/certificates/CertificateController.java
+++ src/main/java/com/example/certificates/CertificateController.java
@@ -5,6 +5,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +17,16 @@ public class CertificateController {
 
     private final TokenService tokenService;
 
+
     @SneakyThrows
     @GetMapping
     public ResponseEntity<String> createToken(CertificateRequest request) {
         return ResponseEntity.ok("JWT Token created successfully: " + tokenService.createToken(request));
+    }
+
+    @PostMapping
+    @RequestMapping("/verify")
+    public ResponseEntity<Boolean> verifyToken(String token) {
+        return ResponseEntity.ok(tokenService.verifyJwtToken(token));
     }
 }
